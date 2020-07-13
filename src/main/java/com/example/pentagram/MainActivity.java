711012f79a85ba408c833e22a7391d97361c7aef
gianfcl <giannfcl@gmail.com>
2020-07-13 18:55:01 -0500

    primer commit

---
+++ src/main/java/com/example/pentagram/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.pentagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //SwipeRefreshLayout sfiMiIndicadorRefresh;
+    //ListView lstMiLista;
+    //Adapter adaptador;
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        //setSupportActionBar(miActionBar);
+
+        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+        listaMascotas.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listaMascotas.setLayoutManager(llm);
+        LlenarMascotas();
+        iniAdaptador();
+
+        /*agregarFAB();
+
+        sfiMiIndicadorRefresh =  (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+        */
+
+    }
+
+    private void iniAdaptador() {
+        MascotaAdaptador adapt = new MascotaAdaptador(mascotas,this);
+        listaMascotas.setAdapter(adapt);
+    }
+
+    public void LlenarMascotas(){
+        mascotas = new ArrayList<Mascota>();
+
+        mascotas.add(new Mascota("Filurais",R.drawable.mascota1,1));
+        mascotas.add(new Mascota("Tom",R.drawable.mascota2,3));
+        mascotas.add(new Mascota("Bunny",R.drawable.mascota3,5));
+    }
+
+    public void refrescandoContenido(){
+        /*
+        sfiMiIndicadorRefresh =  (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+        sfiMiIndicadorRefresh.setRefreshing(false);
+         */
+    }
+
+    public void agregarFAB(){
+        /*
+        FloatingActionButton miFAB=(FloatingActionButton) findViewById(R.id.fabMiFAB);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,getResources().getString(R.string.mensaje),Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR","Click aquí");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
+            }
+        });
+        */
+    }
+}
---
+++ src/main/java/com/example/pentagram/MascotaAdaptador.java
@@ -0,0 +1,72 @@
+package com.example.pentagram;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder>{
+
+    ArrayList<Mascota> mascotas;
+    Activity activity;
+    public MascotaAdaptador(ArrayList<Mascota> mascotas,Activity activity){
+        this.mascotas=mascotas;
+        this.activity=activity;
+    }
+
+    @NonNull
+    @Override
+    public MascotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_mascota,parent,false);
+        return new MascotaViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MascotaViewHolder mascotaViewHolder, final int position) {
+        final Mascota mascota = mascotas.get(position);
+        mascotaViewHolder.nombre.setText(mascota.getNombre());
+        mascotaViewHolder.foto.setImageResource(mascota.getFoto());
+        mascotaViewHolder.like.setText(Integer.toString(mascota.getLike()));
+
+        mascotaViewHolder.hwhite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mascotaViewHolder.like.setText(Integer.toString(mascota.getLike()+1));
+                //Toast.makeText(activity,Integer.toString(mascota.getLike()+1), Toast.LENGTH_LONG).show();
+                mascotas.get(position).setLike(mascotas.get(position).getLike()+1);
+                mascotaViewHolder.like.setText(Integer.toString(mascota.getLike()));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mascotas.size();
+    }
+
+    public static class MascotaViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView nombre;
+        private ImageView foto;
+        private TextView like;
+        private ImageView hwhite;
+        private ImageView hyellow;
+
+        public MascotaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nombre = (TextView) itemView.findViewById(R.id.tvNom);
+            foto = (ImageView) itemView.findViewById(R.id.imgFot);
+            like = (TextView) itemView.findViewById(R.id.tvCont);
+            hwhite = (ImageView) itemView.findViewById(R.id.imgBone1);
+            hyellow = (ImageView) itemView.findViewById(R.id.imgBone2);
+        }
+    }
+}
